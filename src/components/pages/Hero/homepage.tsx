3d790ebbfac866f55f4c4fdc880a52e9eaf0fa12
9Moses <esselmoses12@gmail.com>
2024-07-16 23:12:05 +0000

    upadte crypto

--- src/components/pages/Hero/homepage.tsx
+++ src/components/pages/Hero/homepage.tsx
@@ -1,3 +1,4 @@
+import CryptoCurrencyInfo from "@/components/shared/Crypto/CryptoData";
 import React from "react";
 
 export const Home: React.FC = () => {
@@ -28,100 +29,8 @@ export const Home: React.FC = () => {
             </p>
           </div>
         </div>
-
-        <div className="flex basis-3/5 mt-32">
-          <div className="flex flex-col w-full gap-10">
-            <div className="p-4 rounded-lg bg-white shadow-md md:w-3/5 md:h-full">
-              <div className="flex justify-between">
-                <div>
-                  <div className="flex items-center mb-2">
-                    <img
-                      src="https://cryptologos.cc/logos/tether-usdt-logo.png"
-                      alt="Tether"
-                      className="w-10 h-10 mr-2"
-                    />
-                    <div className="flex flex-col justify-start ">
-                      <span className="text-lg font-semibold">Tether</span>
-                      <p className="text-gray-500 text-sm">USDT</p>
-                    </div>
-                  </div>
-                  <div className="text-3xl font-medium mt-6">$12,715</div>
-                  <div className=" text-red-500 rounded-md bg-red-400/20 w-2/3  mt-1 flex items-center justify-center">
-                    <span className="mr-1">↘</span>
-                    <span className="text-sm">-18%</span>
-                  </div>
-                </div>
-                <div className="mt-2">
-                  <img
-                    src="https://via.placeholder.com/150x50"
-                    alt="Graph"
-                    className="w-full mx-2"
-                  />
-                </div>
-              </div>
-            </div>
-
-            <div className="flex flex-1 gap-2">
-              <div className="p-4 rounded-lg bg-white shadow-md md:w-3/5">
-                <div className="flex justify-between">
-                  <div>
-                    <div className="flex items-center mb-2">
-                      <img
-                        src="https://cryptologos.cc/logos/tether-usdt-logo.png"
-                        alt="Tether"
-                        className="w-10 h-10 mr-2"
-                      />
-                      <div className="flex flex-col justify-start ">
-                        <span className="text-lg font-semibold">Tether</span>
-                        <p className="text-gray-500 text-sm">USDT</p>
-                      </div>
-                    </div>
-                    <div className="text-3xl font-medium mt-6">$12,715</div>
-                    <div className=" text-red-500 rounded-md bg-red-400/20 w-2/3  mt-1 flex items-center justify-center">
-                      <span className="mr-1">↘</span>
-                      <span className="text-sm">-18%</span>
-                    </div>
-                  </div>
-                  <div className="mt-2">
-                    <img
-                      src="https://via.placeholder.com/150x50"
-                      alt="Graph"
-                      className="w-full mx-2"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="p-4 rounded-lg bg-white shadow-md md:w-3/5">
-                <div className="flex justify-between">
-                  <div>
-                    <div className="flex items-center mb-2">
-                      <img
-                        src="https://cryptologos.cc/logos/tether-usdt-logo.png"
-                        alt="Tether"
-                        className="w-10 h-10 mr-2"
-                      />
-                      <div className="flex flex-col justify-start ">
-                        <span className="text-lg font-semibold">Tether</span>
-                        <p className="text-gray-500 text-sm">USDT</p>
-                      </div>
-                    </div>
-                    <div className="text-3xl font-medium mt-6">$12,715</div>
-                    <div className=" text-red-500 rounded-md bg-red-400/20 w-2/3  mt-1 flex items-center justify-center">
-                      <span className="mr-1">↘</span>
-                      <span className="text-sm">-18%</span>
-                    </div>
-                  </div>
-                  <div className="mt-2">
-                    <img
-                      src="https://via.placeholder.com/150x50"
-                      alt="Graph"
-                      className="w-full mx-2"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+        <div className="flex basis-3/5 ">
+          <CryptoCurrencyInfo />
         </div>
       </div>
     </div>
--- src/components/shared/Crypto/CryptoData.tsx
+++ src/components/shared/Crypto/CryptoData.tsx
@@ -1,59 +1,157 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Line } from "react-chartjs-2";
+import {
+  Chart,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+Chart.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 interface CryptoData {
+  id: string;
   name: string;
   symbol: string;
-  price: number;
-  change: number;
+  price_usd: string;
+  percent_change_24h: string;
+  historical_data?: Array<{ date: string; price: number }>;
 }
 
-const CryptoData: React.FC = () => {
-  const [data, setData] = useState<CryptoData | null>(null);
-  const [error, setError] = useState<string | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
+const CryptoCurrencyInfo: React.FC = () => {
+  const [cryptoData, updateCryptoData] = useState<CryptoData[]>([]);
+  const [isFetchingData, updateIsFetchingData] = useState(false);
 
-  const fetchData = async () => {
-    const url = import.meta.env.VITE_CRYPTOCURRENCY;
+  useEffect(() => {
+    fetchCryptoData();
+  }, []);
+
+  const fetchCryptoData = async () => {
+    updateIsFetchingData(true);
     try {
-      const response = await axios.get<CryptoData[]>(url);
-      setData(response.data[0]);
-      setLoading(false);
-    } catch (e) {
-      if (axios.isAxiosError(e)) {
-        setError(e.message);
-      } else {
-        setError("An unknown error occurred.");
-      }
-      setLoading(false);
+      const response = await axios.get("https://api.coinlore.net/api/tickers/");
+      const dataWithHistory = await Promise.all(
+        response.data.data.map(async (crypto: CryptoData) => {
+          const historicalResponse = await axios.get(
+            `https://api.coinlore.net/api/coin/markets/?id=${crypto.id}`
+          );
+          const historicalData = historicalResponse.data.map((entry: any) => ({
+            date: new Date(entry.timestamp * 1000).toLocaleDateString(),
+            price: parseFloat(entry.price_usd),
+          }));
+          return { ...crypto, historical_data: historicalData };
+        })
+      );
+      updateCryptoData(dataWithHistory);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      updateIsFetchingData(false);
     }
   };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
-  if (!data) {
-    return <div>No data available</div>;
-  }
-
   return (
-    <div>
-      <h2>
-        {data.name} ({data.symbol})
-      </h2>
-      <p>Price: ${data.price ? data.price.toLocaleString() : "N/A"}</p>
-      <p>Change: {data.change}%</p>
+    <div className="flex flex-wrap mt-32 justify-center gap-6">
+      {isFetchingData ? (
+        <div className="flex justify-center items-center w-full h-64">
+          <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-gray-900"></div>
+        </div>
+      ) : (
+        cryptoData.slice(0, 1).map((crypto) => (
+          <div
+            key={crypto.id}
+            className="p-4 rounded-lg bg-white shadow-md md:w-[400px] w-full"
+          >
+            <div className="flex justify-between">
+              <div>
+                <div className="flex items-center mb-2">
+                  <img
+                    src={`https://assets.coingecko.com/coins/images/31126/standard/2023-08-11_13.21.24.png?1697025145`}
+                    alt={crypto.name}
+                    className="w-10 h-10 mr-2"
+                  />
+                  <div className="flex flex-col justify-start">
+                    <span className="text-lg font-semibold">{crypto.name}</span>
+                    <p className="text-gray-500 text-sm">{crypto.symbol}</p>
+                  </div>
+                </div>
+                <div className="text-2xl font-medium mt-6">
+                  ${parseFloat(crypto.price_usd).toLocaleString()}
+                </div>
+                <div
+                  className={`rounded-md w-2/3 mt-1 flex items-center justify-center ${
+                    parseFloat(crypto.percent_change_24h) < 0
+                      ? "text-red-500 bg-red-400/20"
+                      : "text-green-500 bg-green-400/20"
+                  }`}
+                >
+                  <span className="mr-1">
+                    {parseFloat(crypto.percent_change_24h) < 0 ? "↘" : "↗"}
+                  </span>
+                  <span className="text-sm">{crypto.percent_change_24h}%</span>
+                </div>
+              </div>
+              <div className="mt-8 w-[150px] h-[100px]  p-4 ">
+                {" "}
+                {/* Adjusted the height for better view */}
+                <Line
+                  data={{
+                    labels:
+                      crypto.historical_data?.map((data) => data.date) ?? [],
+                    datasets: [
+                      {
+                        label: "Price (USD)",
+                        data:
+                          crypto.historical_data?.map((data) => data.price) ??
+                          [],
+                        borderColor:
+                          parseFloat(crypto.percent_change_24h) < 0
+                            ? "#ff0000"
+                            : "#00ff00",
+                        borderWidth: 1,
+                        fill: false,
+                        pointRadius: 1,
+                      },
+                    ],
+                  }}
+                  options={{
+                    responsive: true,
+                    maintainAspectRatio: false,
+                    plugins: {
+                      legend: {
+                        display: false,
+                      },
+                    },
+                    scales: {
+                      x: {
+                        display: false,
+                      },
+                      y: {
+                        display: false,
+                      },
+                    },
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+        ))
+      )}
     </div>
   );
 };
 
-export default CryptoData;
+export default CryptoCurrencyInfo;
